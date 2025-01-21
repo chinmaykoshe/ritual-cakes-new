47a7d377d5c2ec5ed401cacbf2efe330d00b46d1
chinmaykoshe <141853866+chinmaykoshe@users.noreply.github.com>
2025-01-21 23:22:13 +0530

    private route updated

--- ritualcakes/src/PrivateRoute.jsx
+++ ritualcakes/src/PrivateRoute.jsx
@@ -4,9 +4,12 @@ import PropTypes from 'prop-types';
 
 function PrivateRoute({ element }) {
   const role = localStorage.getItem('role'); // Retrieve role from localStorage
+  const email = localStorage.getItem('user'); // Retrieve role from localStorage
+
+  const authorizedEmail = "ritualcake.admin@gmail.com";
 
   // Check if the role is 'admin'
-  if (role === 'admin') {
+  if (role === 'admin' && email === authorizedEmail) {
     return element; // Render the element if user is admin
   }
 
