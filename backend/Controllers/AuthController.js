91820204fc3ea2fd22634e8d8e1309c97de66593
chinmaykoshe <141853866+chinmaykoshe@users.noreply.github.com>
2025-01-23 01:20:26 +0530

    jwt key valid for 1w

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -44,7 +44,7 @@ const login = async (req, res) => {
         const jwtToken = jwt.sign(
             { email: user.email, _id: user._id, role: user.role },
             process.env.JWT_SECRET,
-            { expiresIn: "24h" }
+            { expiresIn: "1w" }
         );
 
         res.status(200).json({
