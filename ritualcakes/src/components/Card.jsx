c40b80740b6f973465329cec6c140e26a68d1f25
chinmaykoshe <141853866+chinmaykoshe@users.noreply.github.com>
2025-11-09 16:08:55 +0530

    Refactor Card component layout for improved structure and readability; adjust price display positioning

--- ritualcakes/src/components/Card.jsx
+++ ritualcakes/src/components/Card.jsx
@@ -6,9 +6,11 @@ function Card({ orderID }) {
   const navigate = useNavigate();
   const allProducts = Object.values(elements).flat();
   const product = allProducts.find((item) => item.orderID === orderID);
+
   if (!product) {
     return <div className="text-center text-red-500">Product not found!</div>;
   }
+
   const handleClick = () => {
     navigate(`/product/${orderID}`);
   };
@@ -18,30 +20,29 @@ function Card({ orderID }) {
       className="group relative bg-white border border-gray-200 shadow-md rounded-lg cursor-pointer"
       onClick={handleClick}
     >
-      <div className="w-full h-48 lg:h-[280px] overflow-hidden rounded-lg">
+      <div className="w-full h-48 lg:h-[280px] overflow-hidden rounded-lg relative">
         <img
           src={product.img}
           alt={product.name}
           onError={(e) => (e.target.src = elements.fallbackImage)}
           className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-95 rounded-lg"
         />
+        <div className="absolute top-2 right-2 transition-transform duration-300 group-hover:scale-110 bg-white/80 backdrop-blur-sm px-2 py-1 rounded-md shadow text-sm font-bold text-gray-800">
+          Rs{" "}
+          {product.prices["500g"] ||
+            product.prices["6 pieces"] ||
+            product.prices["1 jar"] ||
+            product.prices["1 serving"] ||
+            "N/A"}
+        </div>
       </div>
-      <div className="p-4 text-left flex justify-between items-center">
-        <h3 className="text-sm font-semibold text-gray-800 group-hover:text-black-800 group-hover:font-bold transition-colors duration-300">
+      <div className="p-4 text-left">
+        <h3 className="text-sm font-semibold text-gray-800 group-hover:text-black group-hover:font-bold transition-colors duration-300">
           {product.name}
         </h3>
-        <div className="flex items-center space-x-1 text-sm">
-          <span className="text-gray-800 font-bold">Rs</span>
-          <span className="text-gray-800 font-bold">
-            {product.prices["500g"] ||
-              product.prices["6 pieces"] ||
-              product.prices["1 jar"] ||
-              product.prices["1 serving"] ||
-              "N/A"}
-          </span>
-        </div>
       </div>
     </div>
   );
 }
+
 export default Card;
